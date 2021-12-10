b83a5a163cddc43b832fd3df893a14cdd673bd90
Lanka-92791 <92791@fhss.sjp.ac.lk>
2021-12-10 11:29:52 +0700

    bookmarkscreen

--- component/BookMarksScreen.js
+++ component/BookMarksScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Button } from "react-native";
 function BookmarkScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Bookmark screen</Text>
+      <Text>Bookmark scree</Text>
     </View>
   );
 }
