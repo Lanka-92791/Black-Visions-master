dd4b7a88673e7169016174cd4a3cfca31b94d9b9
Lanka-92791 <92791@fhss.sjp.ac.lk>
2021-12-12 22:51:35 +0700

    Support Screen

--- component/SupportScreen.js
+++ component/SupportScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Button } from "react-native";
 function SupportScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Support Screen</Text>
+      <Text>This is Support Screen</Text>
     </View>
   );
 }
