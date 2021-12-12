f4ee5793b8f48b9da0ff4a4c5e49a263f7d594fc
Lanka-92791 <92791@fhss.sjp.ac.lk>
2021-12-12 22:47:42 +0700

    Settings Screen

--- component/SettingsScreen.js
+++ component/SettingsScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Button } from "react-native";
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Settings Screen</Text>
+      <Text>This is Settings Screen</Text>
     </View>
   );
 }
